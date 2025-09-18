417854822b5ef80d9d7974f3fb215a1ca252f194
Nandkishorjadhav <nandkishorjadhav9580@gmail.com>
2025-09-18 09:39:09 +0530

    .

--- server/models/Shop.js
+++ server/models/Shop.js
@@ -3,19 +3,20 @@ const mongoose = require('mongoose');
 const addressSchema = new mongoose.Schema({
   street: { type: String, required: true },
   pincode: { type: String, required: true },
-  village: String,
-  taluka: String,
-  district: String,
+  village: { type: String },
+  taluka: { type: String },
+  district: { type: String },
   state: { type: String, required: true },
   country: { type: String, default: 'India' }
 });
 
 const shopSchema = new mongoose.Schema({
   businessCategory: { type: String, required: true },
+  ownerName: { type: String, required: true },
   shopName: { type: String, required: true },
   shopPhone: { type: String, required: true },
   email: { type: String, required: true },
-  website: String,
+  website: { type: String },
   address: addressSchema
 }, { timestamps: true });
 
